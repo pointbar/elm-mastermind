1711d5922496b898896005bba1a1e6020152a6ff
pointbar <stephane.langlois@scopyleft.fr>
2016-10-18 20:42:41 +0200

    first commit

---
+++ Mastermind.elm
@@ -0,0 +1,51 @@
+module MasterMind exposing (..)
+
+import Html exposing (Html, text, div, ul, li)
+import Html.App exposing (program)
+
+
+-- Main
+
+
+main : Program Never
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Init
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 1, Cmd.none )
+
+
+
+-- Update
+
+
+type Msg = None
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [] [text (toString model)]
---
+++ README.md
@@ -0,0 +1,20 @@
+# Master Mind
+
+## Goal
+A simple [MasterMind](https://en.wikipedia.org/wiki/Mastermind) game in Elm
+
+The program draw a solution, you propose some tries, each time the program evaluate your combination.
+
+## User Stories
+
+### US1 - View the board
+### US2 - Draw a solution
+### US3 - Propose a combination
+### US4 - Evaluate a combination
+### US5 - Propose an other combination
+### US6 - View the solution when game is over
+
+## To compile
+```bash
+$ elm-make Mastermind.elm --output build/masterMind-elm.js
+```
