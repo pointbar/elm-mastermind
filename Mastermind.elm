c4ed0c62fe05cd7471b76d458f410ff9f10a839f
pointbar <stephane.langlois@scopyleft.fr>
2016-10-23 23:56:22 +0200

    Demo

---
+++ Mastermind.elm
@@ -0,0 +1,219 @@
+module MasterMind exposing (..)
+
+import Html exposing (Html, text, div, ul, li)
+import Html.Attributes exposing (class)
+import Html.App exposing (program)
+import Html.Events exposing (onClick)
+import List exposing (..)
+import Random
+
+
+-- Main
+
+
+main : Program Never
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Init
+
+
+type alias Model =
+    { previousTries : List ( Sequence, Evaluation )
+    , currentTry : List Color
+    , solution : Sequence
+    , position : Int
+    , round : Int
+    , isGameOver : Bool
+    }
+
+
+type alias Sequence =
+    List Color
+
+
+type alias Evaluation =
+    List Color
+
+
+type Color
+    = Red
+    | Yellow
+    | Blue
+    | Green
+    | Orange
+    | Purple
+    | Black
+    | White
+    | Nothing
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { previousTries = []
+      , currentTry = []
+      , solution = []
+      , round = 0
+      , position = 1
+      , isGameOver = False
+      }
+    , Random.generate InitSolution <|
+        Random.list 4 <|
+            Random.int 0 7
+    )
+
+
+colorChoices : List Color
+colorChoices =
+    [ Red, Yellow, Blue, Green, Orange, Purple, Black, White ]
+
+
+tries : Model -> List ( Sequence, Evaluation )
+tries model =
+    model.previousTries
+        ++ [ ( model.currentTry ++ (repeat (5 - model.position) Nothing), repeat 4 Nothing ) ]
+        ++ repeat (10 - model.round) ( repeat 4 Nothing, repeat 4 Nothing )
+        |> reverse
+
+
+
+-- Update
+
+
+type Msg
+    = InitSolution (List Int)
+    | SelectColor Color
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        InitSolution random ->
+            ( { model
+                | solution =
+                    map
+                        (\number ->
+                            colorChoices
+                                |> drop number
+                                |> head
+                                |> Maybe.withDefault Nothing
+                        )
+                        random
+              }
+            , Cmd.none
+            )
+
+        SelectColor color ->
+            let
+                currentTry =
+                    model.currentTry ++ [ color ]
+            in
+                case (model.position) of
+                    1 ->
+                        ( { model
+                            | currentTry = currentTry
+                            , position = 2
+                          }
+                        , Cmd.none
+                        )
+
+                    4 ->
+                        let
+                            evaluation =
+                                let
+                                    rightPlace : Int
+                                    rightPlace =
+                                        filter
+                                            (\index ->
+                                                (drop index model.solution |> head) == (drop index currentTry |> head)
+                                            )
+                                            [0..3]
+                                            |> length
+
+                                    wrongPlace : Int
+                                    wrongPlace =
+                                        (filter (\color -> member color model.solution) currentTry
+                                            |> length
+                                        )
+                                            - rightPlace
+                                in
+                                    repeat rightPlace Black
+                                        ++ repeat wrongPlace White
+                                        ++ repeat (4 - rightPlace - wrongPlace) Nothing
+                        in
+                            ( { model
+                                | currentTry = []
+                                , previousTries =
+                                    model.previousTries
+                                        ++ [ ( currentTry, evaluation ) ]
+                                , round = model.round + 1
+                                , position = 1
+                                , isGameOver = evaluation == repeat 4 Black
+                              }
+                            , Cmd.none
+                            )
+
+                    _ ->
+                        ( { model
+                            | currentTry = currentTry
+                            , position = model.position + 1
+                          }
+                        , Cmd.none
+                        )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    let
+        renderSolution : Sequence -> Bool -> Html Msg
+        renderSolution solution isGameOver =
+            case isGameOver of
+                True ->
+                    ul [ class "solution" ] (renderSequence solution)
+
+                False ->
+                    ul [ class "solution" ] (renderSequence (repeat 4 Nothing))
+
+        renderProposition : List ( Sequence, Evaluation ) -> List (Html Msg)
+        renderProposition tries =
+            map
+                (\try ->
+                    ul [ class "round" ]
+                        [ ul [ class "evaluation" ] (renderSequence (snd try))
+                        , ul [ class "choice" ] (renderSequence (fst try))
+                        ]
+                )
+                tries
+
+        renderSequence : Sequence -> List (Html Msg)
+        renderSequence colors =
+            map
+                (\color ->
+                    li [ class (toString color) ] [ text "•" ]
+                )
+                colors
+
+        renderColorChoices : Sequence -> List (Html Msg)
+        renderColorChoices colors =
+            map
+                (\color ->
+                    li [ class (toString color), onClick (SelectColor color) ] [ text "•" ]
+                )
+                colors
+    in
+        div [ class "container" ]
+            [ (renderSolution model.solution model.isGameOver)
+            , ul [ class "propositions" ] (renderProposition (tries model))
+            , ul [ class "color-choices" ] (renderColorChoices colorChoices)
+            ]
